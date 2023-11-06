75e814830e24cc2b4603418643f2fb3a84a23fe6
Daniel Kesseler <dkesseler360@hotmail.com>
2023-11-06 17:12:00 -0500

    committing updates to User Interface in our App

---
+++ app/src/main/java/com/example/myapplication/LogInScreen_Activity.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogInScreen_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_screen);
+    }
+}
